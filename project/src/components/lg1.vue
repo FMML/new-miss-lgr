b68ab3f349da47ede0b6174dc1de2ffabe505da2
Fmm <fanmengmeng@tuhu.cn>
2019-03-28 10:20:44 +0800

    lg

--- project/src/components/lg1.vue
+++ project/src/components/lg1.vue
@@ -11,10 +11,12 @@
         padding:0;
     }
     div{
+        float:right;
         width:100px;
         height:100px;
         background-color: aqua;
-        margin-bottom:20px;
-        bottom:1px solid red;
+        /* margin-bottom:20px; */
+        bottom:1px solid blue;
+        margin:0 auto;
     }
 </style>
